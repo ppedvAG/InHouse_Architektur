23b631af464974ed683b28f2cc323474b83f62d1
André <AndreR@ppedv.de>
2023-05-09 10:29:00 +0200

    Repo definiert und bei EF Implementiert

---
+++ ppedv.CyanBayCars/ppedv.CyanBayCars.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using ppedv.CyanBayCars.Data.EfCore;
+
+namespace ppedv.CyanBayCars.Core
+{
+    public class Class1
+    {
+        public Class1()
+        {
+      
+        }
+    }
+}
---
+++ ppedv.CyanBayCars/ppedv.CyanBayCars.Data.EfCore/EfRepository.cs
@@ -0,0 +1,45 @@
+﻿using ppedv.CyanBayCars.Models;
+using ppedv.CyanBayCars.Models.Contracts;
+
+namespace ppedv.CyanBayCars.Data.EfCore
+{
+    public class EfRepository : IRepository
+    {
+        EfContext context;
+
+        public EfRepository(string conString)
+        {
+            context = new EfContext(conString);
+        }
+
+        public void Add<T>(T entity) where T : Entity
+        {
+            context.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : Entity
+        {
+            context.Remove(entity);
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : Entity
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public T? GetById<T>(int id) where T : Entity
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public int SaveChanges()
+        {
+            return context.SaveChanges();
+        }
+
+        public void Update<T>(T entity) where T : Entity
+        {
+            context.Update(entity);
+        }
+    }
+}
