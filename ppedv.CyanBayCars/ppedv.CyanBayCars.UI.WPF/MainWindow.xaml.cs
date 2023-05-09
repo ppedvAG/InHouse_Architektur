6e44f32b7da8766ad7e6d3cb9e07f24ec83b5139
André <AndreR@ppedv.de>
2023-05-09 17:00:12 +0200

    Hübsches WPF UI

--- ppedv.CyanBayCars/ppedv.CyanBayCars.UI.WPF/MainWindow.xaml.cs
+++ ppedv.CyanBayCars/ppedv.CyanBayCars.UI.WPF/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace ppedv.CyanBayCars.UI.WPF
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow 
     {
         public MainWindow()
         {
