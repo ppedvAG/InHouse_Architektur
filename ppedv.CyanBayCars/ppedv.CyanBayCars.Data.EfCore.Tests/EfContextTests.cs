fc26a5e2e92c381691869a379b2fec8bec75864b
AndreRRR <ruland@outlook.de>
2023-05-09 09:33:15 +0200

    PropertyNameOmmiter

--- ppedv.CyanBayCars/ppedv.CyanBayCars.Data.EfCore.Tests/EfContextTests.cs
+++ ppedv.CyanBayCars/ppedv.CyanBayCars.Data.EfCore.Tests/EfContextTests.cs
@@ -160,5 +160,22 @@ namespace ppedv.CyanBayCars.Data.EfCore.Tests
         }
     }
 
+    internal class PropertyNameOmitter : ISpecimenBuilder
+    {
+        private readonly IEnumerable<string> names;
 
+        internal PropertyNameOmitter(params string[] names)
+        {
+            this.names = names;
+        }
+
+        public object Create(object request, ISpecimenContext context)
+        {
+            var propInfo = request as PropertyInfo;
+            if (propInfo != null && names.Contains(propInfo.Name))
+                return new OmitSpecimen();
+
+            return new NoSpecimen();
+        }
+    }
 }
